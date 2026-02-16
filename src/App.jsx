073acc4c64fdcb9cb276418582f8febaaa05cb63
manjur0 <sfmanjur21@gmail.com>
2026-02-16 22:27:38 +0600

    Implement API call to fetch posts and log response data in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { useEffect } from "react";
 import { getPost } from "./API/PostApi";
 
 const App = () => {
   console.log(getPost());
-  
+
+  // getPostData
+  const getPostData = async () => {
+    try {
+      const response = await getPost();
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // Call data from UseEffect
+  useEffect(() => {
+    getPostData();
+  }, []);
+
   return (
     <div>
       <h1>React CRUD application</h1>
